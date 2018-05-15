460f59f0ba9b2f680134183912bf9383e88c6b19
ranjithum <ranjithum@gmail.com>
2018-05-15 03:07:02 -0400

    Automates profile pus Automates profile pushh

---
+++ push_profile.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+
+'''
+A simple script which automates profile pushes
+'''
+
+import json
+import os
+import getopt
+import sys
+from copy import deepcopy
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import paramiko
+from log_manager import Logger
+
+L_LOGGER_FILE = None
+
+class SshConnection(object):
+    '''
+    A simple class responsible for making a remote
+    ssh connection for the given host-name and user
+    '''
+    def __init__(self, hostname, username, password):
+        self.__hostname = hostname
+        self.__username = username
+        self.__password = password
+        try:
+            self.__host_keys = \
+                    paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+        except IOError:
+            L_LOGGER_FILE.critical("Failed opening the file : ~/.ssh/known_hosts")
+
+        self.__ssh_client = paramiko.SSHClient()
+
+    def connect(self):
+        '''
+        Method which makes the actuall connection
+        to the ssh server
+        '''
+        rsa_pub_key = self.__host_keys[self.__hostname]['ssh-rsa']
+
+        self.__ssh_client.get_host_keys().add(self.__hostname, 'ssh-rsa', rsa_pub_key)
+        self.__ssh_client.connect(self.__hostname, \
+                username=self.__username, \
+                password=self.__password)
+
+    def execute_cmd(self, cmd):
+        '''
+        Method responsible for executing remote command
+        '''
+        self.__ssh_client.exec_command(cmd)
+
+    def execute_cmd_and_exit(self, cmd):
+        '''
+        Method responsible for executing remote command
+        and exit
+        '''
+        self.__ssh_client.exec_command(cmd)
+        self.__ssh_client.close()
+
+    def close_connection(self):
+        '''
+        Method responsible for closing the connection
+        '''
+        self.__ssh_client.close()
+
+def make_full_command(json_dictinary):
+    '''
+    This method is responsible for making the complete
+    command which will be executed in the remote machine
+    '''
+
+    local_copy_json_dict = deepcopy(json_dictinary)
+    screen_name = local_copy_json_dict['screen-name']
+    loc = local_copy_json_dict['location']
+    del local_copy_json_dict['screen-name']
+    del local_copy_json_dict['location']
+
+    screen_cmd = "screen"
+    bash_option = "sh -c"
+
+    tcl_path = ""
+
+    if loc == "blr":
+        tcl_path = "/m/test_main/fwtest/bin/tcl"
+    elif loc == "wtl":
+        tcl_path = "/m/test_main/fwtest/bin/tcl"
+    else:
+        tcl_path = "/m/test_main/fwtest/bin/tcl"
+
+    remaining_attr = ""
+    for key, val in local_copy_json_dict.items():
+        remaining_attr += "-" + str(key) + " " + str(val) + " "
+
+    full_cmd = screen_cmd +  " -dmSL" + " " + screen_name + " " + \
+            bash_option + " '" + tcl_path + " profilemgr " + remaining_attr + "'"
+
+    return full_cmd
+
+# HTTPRequestHandler class
+class HTTPServerRequestHandler(BaseHTTPRequestHandler): # pylint:disable=too-few-public-methods
+    '''
+    Http Server for handling imcoming request
+    '''
+#    @constant
+#    def USERNAME():
+#        return "rgowda"
+#
+#    @constant
+#    def PASSWD():
+#        return "sandvine^123"
+
+    USERNAME = None
+    PASSWD = None
+    BLR_TEST_MACHINE = "blrlab-test-2"
+    WTL_TEST_MACHINE = "wtllab-test-10"
+
+    # GET
+    def do_GET(self): # pylint: disable=invalid-name
+        '''
+        Do Get method impelemented
+        '''
+        # Get the length of the message
+        msg_len = int(self.headers['Content-Length'])
+
+        #Get the request data
+        get_data = self.rfile.read(msg_len).decode()
+        L_LOGGER_FILE.debug(get_data)
+
+        # Get the request data in json object
+        loaded_json = json.loads(get_data)
+
+        # Get all the attributes of json in a dictinary
+        profile_dict = {}
+        for attr in loaded_json:
+            profile_dict[attr] = loaded_json[attr]
+
+        cmd = make_full_command(profile_dict)
+        L_LOGGER_FILE.info("Complete command to execute : ", cmd)
+
+        h_name = ""
+        loc = profile_dict.get("location")
+
+        if loc == "blr":
+            h_name = HTTPServerRequestHandler.BLR_TEST_MACHINE
+        elif loc == "wtl":
+            h_name = HTTPServerRequestHandler.WTL_TEST_MACHINE
+        else:
+            # Log error and return
+            return
+
+        L_LOGGER_FILE.debug("Creating SshConnection object")
+        ssh_c = SshConnection(h_name, \
+                HTTPServerRequestHandler.USERNAME, \
+                HTTPServerRequestHandler.PASSWD)
+
+        L_LOGGER_FILE.info("Connecting to remote server : ", h_name)
+        ssh_c.connect()
+
+        L_LOGGER_FILE.debug("Executing remote command and exiting")
+        ssh_c.execute_cmd_and_exit(cmd)
+
+        L_LOGGER_FILE.debug("Sending Ack back to the client : ", \
+                self.client_address)
+        message = "Acked\n"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+def run():
+    '''
+    Main Run method
+    '''
+    print('starting server...')
+
+    # Server settings
+    server_address = ('0.0.0.0', 8081)
+    httpd = HTTPServer(server_address, HTTPServerRequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+
+def usage():
+    '''
+    Print usage
+    '''
+    print("./push_profile.py \n\
+            -h --help : display help message \n\
+            -u --username : Username to ssh \n\
+            -p --password : password of the user \n\
+            -l --logfile : log file to log\n")
+    sys.exit()
+
+try:
+    OPTS, ARGS = getopt.getopt(sys.argv[1:], "hu:p:l:", ["help", "username=", \
+        "password=", "logfile="])
+except getopt.GetoptError as err:
+    print(err)
+    usage()
+    sys.exit(2)
+
+U_NAME = None
+P_WD = None
+LOG_FILE = None
+
+for opt, value in OPTS:
+    if opt in ("-u", "--username"):
+        U_NAME = value
+    elif opt in ("-p", "--password"):
+        P_WD = value
+    elif opt in ("-l", "--logfile"):
+        LOG_FILE = value
+    elif opt in ("-h", "--help"):
+        usage()
+    else:
+        usage()
+
+if U_NAME is None or P_WD is None or LOG_FILE is None:
+    usage()
+
+HTTPServerRequestHandler.USERNAME = U_NAME
+HTTPServerRequestHandler.PASSWD = P_WD
+
+L_LOGGER_FILE = Logger("Profile-Push", LOG_FILE)
+run()
