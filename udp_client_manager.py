279fa74c093c06c582ddaa0cdd8af8bf552de4aa
ranjithum <ranjithum@gmail.com>
2018-05-21 10:30:07 -0400

    Made changes for receiving miner coins and miner daemons

--- udp_client_manager.py
+++ udp_client_manager.py
@@ -6,12 +6,16 @@ UDP client thread and socket
 import socket
 import sys
 import threading
+import json
 from log_manager import Logger
 
 HELLO_MSG = "Hello"
 ACK_HELLO_MSG = "Ack-Hello"
 KEEP_ALIVE = "Keep-Alive"
 SEND_BASIC = "Send-Basic"
+MINER_DAEMONS = "Miner-Daemons"
+MINER_COINS = "Miner-Coins"
+MINER_DAEMON_PATH = "/opt/cypto"
 
 class UdpSocket(object):
     '''
@@ -34,6 +38,58 @@ class UdpSocket(object):
         self._sock_fd.settimeout(timeout)
         return self._sock_fd.recvfrom(4096)
 
+
+def get_miner_daemons():
+    '''
+    Method to get all the miner daemons that can be
+    deployed
+    '''
+    return next(os.walk(MINER_DAEMON_PATH))[1]
+
+def get_miner_coins():
+    '''
+    Method to get all the coins that are supported by 
+    various miner daemons
+    '''
+    list_of_minerd = get_miner_daemons()
+
+    coins = {}
+    for each_miner in list_of_minerd:
+        miner_etc = MINER_DAEMON_PATH + "/" + each_miner \
+                + "/" + "etc/"
+        coins[each_miner] = next(os.walk(miner_etc))[2]        
+
+    list_of_coins = []
+    for key, values in coins.items():
+        for value in values:
+            if value == "any_config.txt" or value == "any_cpu.txt":
+                continue
+
+            list_of_coins.append(value.split("_")[0])
+
+    return list_of_coins
+
+def get_miner_daemons_json():
+    '''
+    Get the miner daemons in json format
+    {
+        "miner-daemons": "list of daemons"
+    }
+    '''
+    data = {}
+    data['miner-daemons'] = get_miner_daemons()
+    return json.dumps(data)
+
+def get_miner_coins_json():
+    '''
+    Get miner coins in json format
+    {
+        "miner-coins": "list of coins"
+    }
+    '''
+    data = {}
+    data['miner-conis'] = get_miner_coins()
+    return json.dumps(data)
 
 class UdpClientThread(threading.Thread):
     '''
@@ -94,6 +150,14 @@ class UdpClientThread(threading.Thread):
                             critical("Failed to send packet, server might not be running")
                         sys.exit(1)
 
+                elif actual_data == MINER_DAEMONS:
+                    self._logger_ref.debug("Sending Miner Daemons Info")
+                    bytes_sent = self._udp_client_interface.udp_send(get_miner_daemons_json())
+
+                elif actual_data == MINER_COINS:
+                    self._logger_ref.debug("Sending Coins supported by miners")
+                    bytes_sent = self._udp_client_interface.udp_send(get_miner_coins_json())
+
                 elif actual_data == ACK_HELLO_MSG:
                     self._logger_ref.debug("Waiting for server to send the request")
 
@@ -102,8 +166,8 @@ class UdpClientThread(threading.Thread):
 
 '''
 Testing
+'''
 L_LOGGER = Logger("Ranjith", "stdout")
 SERVER_ADD = ("localhost", 6767)
 UDPCLIENT_MANAGER = UdpClientThread(SERVER_ADD, L_LOGGER)
 UDPCLIENT_MANAGER.start()
-'''
